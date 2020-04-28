b0905d0d2d377b2009535b397b99f9452ab96701
Kc Kai <kirstenckai@gmail.com>
2020-04-28 20:02:40 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require './math-game'
+
+binding.pry
+
+puts "Hello world!"
---
+++ math-game.rb
@@ -0,0 +1,25 @@
+
+
+# class Players(name)
+# Will take a players name
+# Will keep track of each players score
+# decrease score
+
+# class Game 
+# This class handles the looping
+
+# Starts a new game
+# Keeps track of turns
+# This class could maintain the I/O of the players
+# keep track of players
+# If the answer is equal to the sum of the two numbers, then the player is correct
+# If the answer is not equal to the sum of the two numbers, then the player is wrong and their score goes down by one
+
+# class Questions
+# Generate the questions
+# Keep track of the right answer
+# check: boolean
+
+
+
+
