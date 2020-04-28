0b983cbc868bb55dc7f1776253ece6f86a85ab64
Kc Kai <kirstenckai@gmail.com>
2020-04-28 23:20:11 +0000

    Create Game class

--- main.rb
+++ main.rb
@@ -1,12 +1,57 @@
-require 'pry'
+
 require './math-game'
+# class Game 
+# This class handles the looping
+# Keeps track of turns
+# This class could maintain the I/O of the players
+# keep track of players
+# If the answer is equal to the sum of the two numbers, then the player is correct
+# If the answer is not equal to the sum of the two numbers, then the player is wrong and their score goes down by one
 
-binding.pry
+class Game
+  player_1 = Player.new(3, 1)
+  player_2 = Player.new(3, 2)
 
-p1 = Player.new(2, 1)
-puts p1.name
-puts p1.score
+  puts 'Math Game'
+  puts player_1.name, player_1.score
+  puts player_2.name, player_2.score
+  
+  turns = 0
+  while player_1.score > 0 && player_2.score > 0
+    turns += 1
+    generate_question = Questions.new
+    answer = generate_question.answer
+    puts '--------- NEW TURN ------------'
+    if turns.odd?
+      puts "Player 1: " + generate_question.question
+    else
+      puts "Player 2: " + generate_question.question
+    end
 
-test = Questions.new()
-puts test.question
-puts test.answer
+    user_input = gets.chomp
+
+    if user_input.to_i == answer
+      puts "YES! You are correct."
+    else
+      if turns.odd?
+        player_1.decrease_score
+      else 
+        player_2.decrease_score
+      end
+      puts "Incorrect!!!"
+    end
+
+    puts "P1: #{player_1.score}/3 vs P2: #{player_2.score}/3"
+  end
+
+  if player_1.score == 0
+    puts "Player 2 wins with a score of #{player_2.score}/3"
+  else
+    puts "Player 1 wins with a score of #{player_1.score}/3"
+  end
+
+end
+
+# test = Questions.new()
+# puts test.question
+# puts test.answer
--- math-game.rb
+++ math-game.rb
@@ -3,7 +3,7 @@ class Player
   attr_accessor :name, :score
 
   def initialize(score, num)
-    @score = "#{score}/3"
+    @score = score
     @name = "Player #{num}"
   end
 
@@ -12,21 +12,10 @@ class Player
   end
 end
 
-# class Game 
-# This class handles the looping
-
-# Starts a new game
-# Keeps track of turns
-# This class could maintain the I/O of the players
-# keep track of players
-# If the answer is equal to the sum of the two numbers, then the player is correct
-# If the answer is not equal to the sum of the two numbers, then the player is wrong and their score goes down by one
-
 # class Questions
 # Generate the questions
 # Keep track of the right answer
 # check: boolean
-
 class Questions
   attr_accessor :question, :answer, :num_1, :num_2
   def initialize
